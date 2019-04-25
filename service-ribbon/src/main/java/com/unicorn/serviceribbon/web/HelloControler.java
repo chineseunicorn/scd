342542ad27cfb2df4f632dbc94250b0049e5b6cf
allen <wangjilin>
2019-04-25 17:15:01 +0800

    first commit

---
+++ service-ribbon/src/main/java/com/unicorn/serviceribbon/web/HelloControler.java
@@ -0,0 +1,32 @@
+package com.unicorn.serviceribbon.web;
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import com.unicorn.serviceribbon.service.HelloService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloControler {
+
+    @Autowired
+    HelloService helloService;
+
+    @GetMapping(value = "/hi")
+    public String hi(@RequestParam String name) {
+        return helloService.hiService( name );
+    }
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+}
