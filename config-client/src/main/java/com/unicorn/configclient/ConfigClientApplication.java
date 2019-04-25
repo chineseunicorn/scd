4e30a99d9441eb3d889a151be24cc3a764f51c20
allen <wangjilin>
2019-04-25 18:29:40 +0800

    高可用分布式配置中心完善 1.config-server 2.config-client 3.config-eureka-server

--- config-client/src/main/java/com/unicorn/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/unicorn/configclient/ConfigClientApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
 @RestController
+@SpringBootApplication
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
