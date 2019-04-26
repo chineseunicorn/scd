e2209cb930031f9081b0a7c42706a39d44650cdc
allen <wangjilin>
2019-04-26 17:05:14 +0800

    消息总线 Spring Cloud Bus RibbitMq

--- config-client/src/main/java/com/unicorn/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/unicorn/configclient/ConfigClientApplication.java
@@ -3,12 +3,22 @@ package com.unicorn.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableEurekaClient
+@EnableDiscoveryClient
+@RefreshScope
 @RestController
 @SpringBootApplication
 public class ConfigClientApplication {
+
+    /**
+     * http://localhost:8881/actuator/bus-refresh
+     */
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
